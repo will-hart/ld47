ed215f4cec7222d90119bccb21ee4ec6c3da7042
Will Hart <hart.wl@gmail.com>
2020-10-05 23:23:36 +1100

    Rename button

--- src/abilities/systems.rs
+++ src/abilities/systems.rs
@@ -3,9 +3,11 @@ use crate::{components::*, events::RedrawAbilityUiEvent};
 use ability_data::AbilityDatabase;
 use bevy::prelude::*;
 use spectre_core::{Health, Mana, Movement};
+use spectre_time::GameTime;
 
 pub fn ability_purchase_system(
     mut commands: Commands,
+    game_time: Res<GameTime>,
     mut abilities: ResMut<AbilityDatabase>,
     mut player_score: ResMut<PlayerScore>,
     mut ability_redraw_event: ResMut<Events<RedrawAbilityUiEvent>>,
@@ -69,19 +71,32 @@ pub fn ability_purchase_system(
                     AbilityDetail::Buff(detail) => {
                         match detail.buff_type {
                             BuffType::Armour => {
+                                println!("Buffing base_armour");
                                 defence.base_armour.buffs.push(detail.buff.clone());
+                                defence.base_armour.update(game_time.elapsed_time);
                             }
                             BuffType::Health => {
+                                println!("Buffing max_health");
                                 health.max_health.buffs.push(detail.buff.clone());
+                                health.max_health.update(game_time.elapsed_time);
+                                health.current_health = health.max_health.value;
+                                health.target_health = health.current_health;
                             }
                             BuffType::Mana => {
+                                println!("Buffing max_mana");
                                 mana.max_mana.buffs.push(detail.buff.clone());
+                                mana.max_mana.update(game_time.elapsed_time);
+                                mana.current_mana = mana.max_mana.value;
                             }
                             BuffType::Regeneration => {
+                                println!("Buffing regeneration");
                                 health.regeneration.buffs.push(detail.buff.clone());
+                                health.regeneration.update(game_time.elapsed_time);
                             }
                             BuffType::MovementSpeed => {
+                                println!("Buffing movement_speed");
                                 movement.movement_speed.buffs.push(detail.buff.clone());
+                                movement.movement_speed.update(game_time.elapsed_time);
                             }
                         };
                     }
